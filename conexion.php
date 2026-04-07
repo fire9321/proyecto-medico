0bda9bd8d39f3ef823ed60ef99ae0b9217566d86
fire9321 <88172705+fire9321@users.noreply.github.com>
2026-04-07 17:03:43 -0400

    conexion.php

--- conexion.php
+++ conexion.php
@@ -1,12 +1,12 @@
 <?php
 $host = "localhost";
 $user = "root"; // Usuario por defecto
-$pass = "";     // Deja vacío si es XAMPP, o pon tu clave de Workbench
-$db   = "fichas"; // Nombre que pusiste en el Paso 1
+$pass = "";     // Deja vacío si es XAMPP
+$db   = "fichas"; 
 
 $con = mysqli_connect($host, $user, $pass, $db);
 
-// Verificación para tu informe de V&V
+
 if (!$con) {
     die("Error de conexión: " . mysqli_connect_error());
 }
